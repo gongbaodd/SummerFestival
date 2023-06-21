81814ed14431c5888f44a0e9a286648886528cc7
gongbaodd <gongbaodd@Inspiron.Ubuntu>
2023-06-21 19:24:52 +0800

    moveDirection

--- src/Components/Keyboard.tsx
+++ src/Components/Keyboard.tsx
@@ -1,95 +1,119 @@
 import { ActionManager, ExecuteCodeAction, Scalar } from "@babylonjs/core";
-import { signal } from "@preact/signals-react";
-import React, { FC, createContext, useCallback, useMemo, useState } from "react";
+import { signal, useSignal } from "@preact/signals-react";
+import React, {
+  FC,
+  createContext,
+  useCallback,
+  useMemo,
+  useState,
+} from "react";
 import { useBeforeRender, useScene } from "react-babylonjs";
 
-const keyMap = new Map<string, boolean>()
-
-const vertical = signal(0)
-const verticalAxis = signal(0)
-const horizontal = signal(0)
-const horizontalAxis = signal(0)
-
-const dashing = signal(false)
-
-const jumKeyDown = signal(false)
-
-const updateFromKeybaord = () => {
-    if (keyMap.get("ArrowUp")) {
-        vertical.value = Scalar.Lerp(vertical.value, 1, 0.2)
-        verticalAxis.value = 1
-    } else if (keyMap.get("ArrowDown")) {
-        vertical.value = Scalar.Lerp(vertical.value, -1, 0.2)
-        verticalAxis.value = -1
-    } else {
-        vertical.value = 0
-        verticalAxis.value = 0
-    }
-
-    if (keyMap.get("ArrowLeft")) {
-        horizontal.value = Scalar.Lerp(horizontal.value, -1, 0.2)
-        horizontalAxis.value = -1
-    } else if (keyMap.get("ArrowRight")) {
-        horizontal.value = Scalar.Lerp(horizontal.value, 1, 0.2)
-        horizontalAxis.value = 1
-    } else {
-        horizontal.value = 0
-        horizontalAxis.value = 0
-    }
-
-    if (keyMap.get("Shift")) {
-        dashing.value = true
-    } else {
-        dashing.value = false
-    }
-
-    if (keyMap.get(" ")) {
-        jumKeyDown.value = true
-    } else {
-        jumKeyDown.value = false
-    }
-}
-
-const keyDown = new ExecuteCodeAction(ActionManager.OnKeyDownTrigger, (evt) => {
-    keyMap.set(evt.sourceEvent.key, evt.sourceEvent.type === "keydown")
-});
-const keyUp = new ExecuteCodeAction(ActionManager.OnKeyUpTrigger, (evt) => {
-    keyMap.set(evt.sourceEvent.key, evt.sourceEvent.type === "keydown")
-});
-
 interface Props {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
-const states = {
-    vertical,
-    verticalAxis,
-    horizontal,
-    horizontalAxis,
-    dashing,
-    jumKeyDown,
+const INIT_STATES = {
+  vertical: 0,
+  verticalAxis: 0,
+  horizontal: 0,
+  horizontalAxis: 0,
+  dashing: false,
+  jumKeyDown: false,
 };
 
 interface IContext {
-    states: typeof states
+  Context: React.Context<typeof INIT_STATES>;
 }
 
 export const Keyboard: FC<Props> & IContext = function ({ children }) {
-    const scene = useScene()
+  const scene = useScene();
+  const keyMap = useMemo(() => new Map<string, boolean>(), []);
 
-    useMemo(() => {
-        if (!scene) return
-        const am = new ActionManager(scene)
-        am.registerAction(keyDown)
-        am.registerAction(keyUp)
-        return am
-    }, [scene])
+  const vertical = useSignal(0);
+  const verticalAxis = useSignal(0);
+  const horizontal = useSignal(0);
+  const horizontalAxis = useSignal(0);
+  const dashing = useSignal(false);
+  const jumKeyDown = useSignal(false);
 
-    useBeforeRender(() => {
-        updateFromKeybaord()
-    })
+  useMemo(() => {
+    if (!scene) return;
+    const am = new ActionManager(scene);
+    am.registerAction(keyDown);
+    am.registerAction(keyUp);
+    return am;
+  }, [scene]);
 
-    return children;
-}
+  const updateFromKeybaord = () => {
+    if (keyMap.get("ArrowUp")) {
+      vertical.value = Scalar.Lerp(vertical.value, 1, 0.2);
+      verticalAxis.value = 1;
+    } else if (keyMap.get("ArrowDown")) {
+      vertical.value = Scalar.Lerp(vertical.value, -1, 0.2);
+      verticalAxis.value = -1;
+    } else {
+      vertical.value = 0;
+      verticalAxis.value = 0;
+    }
 
-Keyboard.states = states
+    if (keyMap.get("ArrowLeft")) {
+      horizontal.value = Scalar.Lerp(horizontal.value, -1, 0.2);
+      horizontalAxis.value = -1;
+    } else if (keyMap.get("ArrowRight")) {
+      horizontal.value = Scalar.Lerp(horizontal.value, 1, 0.2);
+      horizontalAxis.value = 1;
+    } else {
+      horizontal.value = 0;
+      horizontalAxis.value = 0;
+    }
+
+    if (keyMap.get("Shift")) {
+      dashing.value = true;
+    } else {
+      dashing.value = false;
+    }
+
+    if (keyMap.get(" ")) {
+      jumKeyDown.value = true;
+    } else {
+      jumKeyDown.value = false;
+    }
+  };
+
+  const keyDown = useMemo(
+    () =>
+      new ExecuteCodeAction(ActionManager.OnKeyDownTrigger, (evt) => {
+        keyMap.set(evt.sourceEvent.key, evt.sourceEvent.type === "keydown");
+      }),
+    []
+  );
+  const keyUp = useMemo(
+    () =>
+      new ExecuteCodeAction(ActionManager.OnKeyUpTrigger, (evt) => {
+        keyMap.set(evt.sourceEvent.key, evt.sourceEvent.type === "keydown");
+      }),
+    []
+  );
+
+  useBeforeRender(() => {
+    updateFromKeybaord();
+  });
+
+  return (
+    <Keyboard.Context.Provider
+      value={{
+        vertical: vertical.value,
+        verticalAxis: verticalAxis.value,
+        horizontal: horizontal.value,
+        horizontalAxis: horizontalAxis.value,
+        dashing: dashing.value,
+        jumKeyDown: jumKeyDown.value,
+      }}
+    >
+      {children}
+    </Keyboard.Context.Provider>
+  );
+};
+
+Keyboard.Context = createContext(INIT_STATES);
--- src/Components/Player.tsx
+++ src/Components/Player.tsx
@@ -1,131 +1,177 @@
-import { Color3, Matrix, MeshBuilder, Quaternion, Vector3, StandardMaterial, TransformNode, UniversalCamera, Mesh, Nullable, ShadowGenerator } from "@babylonjs/core"
-import { FC, forwardRef, useCallback, useContext, useEffect, useMemo, useRef } from "react"
-import { useBeforeRender, useScene } from "react-babylonjs"
-import { setForwardRef } from "../Helpers/ref"
-import { Keyboard } from "./Keyboard"
-import { computed, effect, signal, useSignal } from "@preact/signals-react"
+import {
+  Color3,
+  Matrix,
+  MeshBuilder,
+  Quaternion,
+  Vector3,
+  StandardMaterial,
+  TransformNode,
+  UniversalCamera,
+  Mesh,
+  Nullable,
+  ShadowGenerator,
+} from "@babylonjs/core";
+import {
+  FC,
+  forwardRef,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+} from "react";
+import { useBeforeRender, useScene } from "react-babylonjs";
+import { setForwardRef } from "../Helpers/ref";
+import { Keyboard } from "./Keyboard";
+import {
+  computed,
+  effect,
+  signal,
+  useComputed,
+  useSignal,
+} from "@preact/signals-react";
 
-const PLAYER_SPEED = 0.45
-const JUMP_FORCE = 0.8
-const GRAVITY = -2.8
-const DASH_FACTOR = 2.5
-const DASH_TIME = 10
-const DOWN_TILT = new Vector3(0.8290313946973066, 0, 0)
-const ORINGINAL_TILT = new Vector3(0.5934119456780721, 0, 0)
+const PLAYER_SPEED = 0.45;
+const JUMP_FORCE = 0.8;
+const GRAVITY = -2.8;
+const DASH_FACTOR = 2.5;
+const DASH_TIME = 10;
+const DOWN_TILT = new Vector3(0.8290313946973066, 0, 0);
+const ORINGINAL_TILT = new Vector3(0.5934119456780721, 0, 0);
 
-const camPos = new Vector3(0, 0, -30)
+const camPos = new Vector3(0, 0, -30);
 
-const moveDirection = signal(Vector3.Zero())
-
-const horizontal = signal(0)
-const vertical = signal(0)
-
-const fwd = signal(Vector3.Zero())
-const right = signal(Vector3.Zero())
-const correctedVertical = computed(() => {
-    return fwd.value.scaleInPlace(vertical.value)
-})
-const correctedHorizontal = computed(() => {
-    return right.value.scaleInPlace(horizontal.value)
-})
-const move = computed(() => {
-    return correctedHorizontal.value.addInPlace(correctedVertical.value)
-})
-const inpuAmt = computed(() => {
-    const inputMag = Math.abs(horizontal.value) + Math.abs(vertical.value)
-    if (inputMag > 1) return 1
-    if (inputMag < 0) return 0
-    return inputMag
-})
-
-effect(() => {
-    moveDirection.value = moveDirection.value.scaleInPlace(inpuAmt.value * PLAYER_SPEED)
-})
 interface Props {
-    shadow: Nullable<ShadowGenerator>
+  shadow: Nullable<ShadowGenerator>;
 }
 
 export const Player: FC<Props> = ({ shadow }) => {
-    const scene = useScene()
+  const scene = useScene();
 
-    const cameraRef = useRef<UniversalCamera | null>(null)
+  const cameraRef = useRef<UniversalCamera | null>(null);
 
-    const player = useMemo(() => {
-        if (!scene) return
+  const moveDirection = useSignal(Vector3.Zero());
 
-        const outer = MeshBuilder.CreateBox("collider", { width: 2, depth: 1, height: 3 }, scene)
-        outer.isVisible = false
-        outer.isPickable = false
-        outer.checkCollisions = true
+  const horizontal = useSignal(0);
+  const vertical = useSignal(0);
 
-        outer.bakeTransformIntoVertices(Matrix.Translation(0, 1.5, 0))
+  const fwd = useSignal(Vector3.Zero());
+  const right = useSignal(Vector3.Zero());
+  const correctedVertical = useComputed(() => {
+    return fwd.value.scaleInPlace(vertical.value);
+  });
+  const correctedHorizontal = useComputed(() => {
+    return right.value.scaleInPlace(horizontal.value);
+  });
+  const move = useComputed(() => {
+    return correctedHorizontal.value.addInPlace(correctedVertical.value);
+  });
+  const inpuAmt = useComputed(() => {
+    const inputMag = Math.abs(horizontal.value) + Math.abs(vertical.value);
+    if (inputMag > 1) return 1;
+    if (inputMag < 0) return 0;
+    return inputMag;
+  });
 
-        outer.ellipsoid = new Vector3(1, 1.5, 1)
-        outer.ellipsoidOffset = new Vector3(0, 1.5, 0)
+  // TODO
+  moveDirection.value = moveDirection.value.scaleInPlace(
+    inpuAmt.value * PLAYER_SPEED
+  );
 
-        outer.rotationQuaternion = new Quaternion(0, 1, 0, 0);
+  const player = useMemo(() => {
+    if (!scene) return;
 
-        shadow?.addShadowCaster(outer)
+    const outer = MeshBuilder.CreateBox(
+      "player",
+      { width: 2, depth: 1, height: 3 },
+      scene
+    );
+    outer.isVisible = false;
+    outer.isPickable = false;
+    outer.checkCollisions = true;
 
-        return outer
-    }, [scene, shadow])
-    const body = useMemo(() => {
-        if (!scene) return
+    outer.bakeTransformIntoVertices(Matrix.Translation(0, 1.5, 0));
 
-        const body = MeshBuilder.CreateCylinder("body", {
-            height: 3,
-            diameterTop: 2,
-            diameterBottom: 2,
-            tessellation: 0,
-            subdivisions: 0
-        }, scene)
-        const material = new StandardMaterial("ref", scene)
-        material.diffuseColor = new Color3(0.8, 0.5, 0.5)
+    outer.ellipsoid = new Vector3(1, 1.5, 1);
+    outer.ellipsoidOffset = new Vector3(0, 1.5, 0);
 
-        body.material = material
-        body.isPickable = false
-        body.bakeTransformIntoVertices(Matrix.Translation(0, 1.5, 0))
+    outer.rotationQuaternion = new Quaternion(0, 1, 0, 0);
 
-        return body
-    }, [scene])
-    const inner = useMemo(() => {
-        if (!scene) return
-        const box = MeshBuilder.CreateBox("inner", { width: 0.5, depth: 0.5, height: 0.25 }, scene)
-        return box
-    }, [scene])
-    const camRoot = useMemo(() => {
-        const root = new TransformNode("CamRoot")
-        root.position = new Vector3(0, 0, 0)
-        root.rotation = new Vector3(0, Math.PI, 0)
-        return root
-    }, [])
-    const yTilt = useMemo(() => {
-        const yTilt = new TransformNode("yTilt")
-        // yTilt.rotation = Player.ORIGINAL_TILT
-        return yTilt
-    }, [])
+    shadow?.addShadowCaster(outer);
 
-    useEffect(() => {
-        if (!scene) return
-        if (!cameraRef.current) return
-        scene.activeCamera = cameraRef.current
-        camRoot.position = Vector3.Lerp(camRoot.position, player.position, 0.4)
-    }, [scene, camRoot])
+    return outer;
+  }, [scene, shadow]);
+  const body = useMemo(() => {
+    if (!scene) return;
 
-    return (
-        <>
-            <abstractMesh name="player" fromInstance={player}>
-                <abstractMesh name="body" fromInstance={body}>
-                    <abstractMesh name="inner" fromInstance={inner} />
-                </abstractMesh>
-            </abstractMesh>
-            <transformNode name="camRoot" fromInstance={camRoot} >
-                <universalCamera ref={cameraRef} name="playerCam" position={camPos} fov={.5} lockedTarget={camRoot.position} >
-                    <transformNode name="yTilt" fromInstance={yTilt} />
-                </universalCamera>
-            </transformNode>
-        </>
-    )
-}
+    const body = MeshBuilder.CreateCylinder(
+      "body",
+      {
+        height: 3,
+        diameterTop: 2,
+        diameterBottom: 2,
+        tessellation: 0,
+        subdivisions: 0,
+      },
+      scene
+    );
+    const material = new StandardMaterial("ref", scene);
+    material.diffuseColor = new Color3(0.8, 0.5, 0.5);
 
-Player.displayName = "Player"
+    body.material = material;
+    body.isPickable = false;
+    body.bakeTransformIntoVertices(Matrix.Translation(0, 1.5, 0));
+
+    return body;
+  }, [scene]);
+  const inner = useMemo(() => {
+    if (!scene) return;
+    const box = MeshBuilder.CreateBox(
+      "inner",
+      { width: 0.5, depth: 0.5, height: 0.25 },
+      scene
+    );
+    return box;
+  }, [scene]);
+  const camRoot = useMemo(() => {
+    const root = new TransformNode("CamRoot");
+    root.position = new Vector3(0, 0, 0);
+    root.rotation = new Vector3(0, Math.PI, 0);
+    return root;
+  }, []);
+  const yTilt = useMemo(() => {
+    const yTilt = new TransformNode("yTilt");
+    // yTilt.rotation = Player.ORIGINAL_TILT
+    return yTilt;
+  }, []);
+
+  useEffect(() => {
+    if (!scene) return;
+    if (!cameraRef.current) return;
+    scene.activeCamera = cameraRef.current;
+    camRoot.position = Vector3.Lerp(camRoot.position, player.position, 0.4);
+  }, [scene, camRoot]);
+
+  return (
+    <>
+      <abstractMesh name="player" fromInstance={player}>
+        <abstractMesh name="body" fromInstance={body}>
+          <abstractMesh name="inner" fromInstance={inner} />
+        </abstractMesh>
+      </abstractMesh>
+      <transformNode name="camRoot" fromInstance={camRoot}>
+        <universalCamera
+          ref={cameraRef}
+          name="playerCam"
+          position={camPos}
+          fov={0.5}
+          lockedTarget={camRoot.position}
+        >
+          <transformNode name="yTilt" fromInstance={yTilt} />
+        </universalCamera>
+      </transformNode>
+    </>
+  );
+};
+
+Player.displayName = "Player";
